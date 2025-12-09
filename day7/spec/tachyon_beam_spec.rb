ec99633285c4022b20c6ea86aa48706f8fcb2b98
Karl Keller <kgk86@hotmail.com>
2025-12-09 10:16:06 +0100

    Day7 - initial test case

---
+++ day7/spec/tachyon_beam_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../tachyon_beam.rb"
+
+simple_map    =  "....S....
+                  ....|....
+                  ...|^|...
+                  ...|.|..."
+
+
+example_map   = [['.','.','.','.','S','.','.','.','.'],
+                 ['.','.','.','.','|','.','.','.','.'],
+                 ['.','.','.','|','^','|','.','.','.'],
+                 ['.','.','.','|','.','|','.','.','.']
+                ]
+
+describe "Teleportation Laboratory" do
+  describe "#create_map_from_string" do
+    it "creates a 2d array from the string input" do
+      expect(create_map_from_string(simple_map)).to eq(example_map)
+    end
+  end
+end
